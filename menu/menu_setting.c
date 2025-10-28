e606d4b1d28931e5ab4b8d8a9cc5375d5b05edc7
Matt <matt@matt.buzz>
2025-10-28 00:01:35 -0700

    Also change menu default to false for consistency
    Both configuration.c and menu_setting.c should have the same default
    value for shader_gyro_enable (false).

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -14172,7 +14172,7 @@ static bool setting_append_list(
                   &settings->bools.shader_gyro_enable,
                   MENU_ENUM_LABEL_SHADER_GYRO_ENABLE,
                   MENU_ENUM_LABEL_VALUE_SHADER_GYRO_ENABLE,
-                  true,
+                  false,
                   MENU_ENUM_LABEL_VALUE_OFF,
                   MENU_ENUM_LABEL_VALUE_ON,
                   &group_info,
