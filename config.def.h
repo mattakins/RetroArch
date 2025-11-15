80e9414bb9054719c313880435b0a90350c43e55
Matt <matt@matt.buzz>
2025-11-14 19:52:09 -0800

    Use HAVE_SENSORS flag for sensor default
    Changed DEFAULT_INPUT_SENSORS_ENABLE to check HAVE_SENSORS instead of
    ANDROID. This enables sensors by default on all platforms with sensor
    support (Android, Web, PS3), not just Android specifically.
    
    Also fixes bug where both conditional branches were set to true, which
    would have incorrectly enabled sensors on desktop builds without sensor
    support.

--- config.def.h
+++ config.def.h
@@ -1626,13 +1626,13 @@
 
 /* Enables accelerometer/gyroscope/illuminance
  * sensor input, if supported */
-#if defined(ANDROID)
-/* Enable sensors by default for motion-based shader effects.
+/* Sensors enabled by default on platforms with sensor support.
  * Battery impact is minimal on modern devices (~1-3mA).
  * Sensors auto-disable when app is backgrounded. */
+#if defined(HAVE_SENSORS)
 #define DEFAULT_INPUT_SENSORS_ENABLE true
 #else
-#define DEFAULT_INPUT_SENSORS_ENABLE true
+#define DEFAULT_INPUT_SENSORS_ENABLE false
 #endif
 
 /* Automatically enable game focus when running or
