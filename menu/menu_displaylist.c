cf1182b66d0dfb847e43065a57603eb0c216e80c
Matt <matt@matt.buzz>
2025-11-28 20:10:18 -0800

    Add accelerometer orientation to sensor settings menu
    The setting was defined but not appearing in the Motion/Light Sensors
    submenu because it was missing from menu_displaylist.c.

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -7523,6 +7523,10 @@ unsigned menu_displaylist_build_list(
                      PARSE_ONLY_FLOAT, false) == 0)
                count++;
             if (MENU_DISPLAYLIST_PARSE_SETTINGS_ENUM(list,
+                     MENU_ENUM_LABEL_INPUT_SENSOR_ACCELEROMETER_ORIENTATION,
+                     PARSE_ONLY_UINT, false) == 0)
+               count++;
+            if (MENU_DISPLAYLIST_PARSE_SETTINGS_ENUM(list,
                      MENU_ENUM_LABEL_INPUT_SENSOR_GYROSCOPE_SENSITIVITY,
                      PARSE_ONLY_FLOAT, false) == 0)
                count++;
